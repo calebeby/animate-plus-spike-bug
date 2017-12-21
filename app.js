22b6aac1c7fb3cd0245b02034a25d8778adac84c
Caleb Eby <caleb.eby01@gmail.com>
2017-12-20 17:07:34 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const htmlStandards = require("reshape-standard");
+const cssStandards = require("spike-css-standards");
+const jsStandards = require("spike-js-standards");
+const pageId = require("spike-page-id");
+const env = process.env.SPIKE_ENV;
+const restSpread = require("babel-plugin-transform-object-rest-spread");
+
+module.exports = {
+  devtool: "source-map",
+  ignore: [
+    "**/layout.html",
+    "**/_*",
+    "**/.*",
+    "readme.md",
+    "yarn.lock",
+    "package-lock.json"
+  ],
+  reshape: htmlStandards({
+    locals: ctx => {
+      return { pageId: pageId(ctx), foo: "bar" };
+    },
+    minify: env === "production"
+  }),
+  postcss: cssStandards({
+    minify: env === "production",
+    warnForDuplicates: env !== "production"
+  }),
+  babel: jsStandards({
+    appendPlugins: restSpread
+  })
+};
---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+import animatePlus from "animateplus";
+
+// const addAnimations = (options, resolve) => {
+//   const {
+//     elements = null,
+//     easing = "out-elastic",
+//     duration = 1000,
+//     delay: timeout = 0,
+//     speed = 1,
+//     loop = false,
+//     optimize = false,
+//     direction = "normal",
+//     change = null,
+//     ...rest
+//   } = options;
+// };
+
+// addAnimations({ foo: "hi", bar: "hi" });
